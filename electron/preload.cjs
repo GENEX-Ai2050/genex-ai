cb51445cc6534479789ebc681648d9c384e82e7d
GENEX-Ai2050 <genex.ai2050@gmail.com>
2026-04-13 11:38:59 +0300

    Update preload.cjs

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -2,5 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('genexDesktop', {
   openUrl: (url) => ipcRenderer.invoke('open-url', url),
-  copyText: (text) => ipcRenderer.invoke('copy-text', text)
+  copyText: (text) => ipcRenderer.invoke('copy-text', text),
+  createRealtimeSession: (payload) => ipcRenderer.invoke('genex:create-realtime-session', payload),
+  showFounder: (founderImageUrl) => ipcRenderer.invoke('genex:show-founder', founderImageUrl)
 });

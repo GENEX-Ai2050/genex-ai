8ef79a5187c5467d0d30be254d642e82e4a2ad1c
GENEX-Ai2050 <genex.ai2050@gmail.com>
2026-04-13 10:04:50 +0300

    Update preload.cjs

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,1 +1,6 @@
+const { contextBridge, ipcRenderer } = require('electron');
 
+contextBridge.exposeInMainWorld('genexDesktop', {
+  openUrl: (url) => ipcRenderer.invoke('open-url', url),
+  copyText: (text) => ipcRenderer.invoke('copy-text', text)
+});
